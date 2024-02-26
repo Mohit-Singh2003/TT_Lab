6751498a138afe377f0f270c94d08ff928a35653
Mohit Singh <91059735+Mohit-Singh2003@users.noreply.github.com>
2024-02-26 05:11:39 +0000

    again

--- 1.cpp
+++ 1.cpp
@@ -1,20 +1,22 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-struct ListNode{
-    char val;
-    ListNode* next;
-    ListNode(char x) : val(x),
-    next(nullptr){}
-};
-bool isPalindrome(ListNode* head){
-    if(!head || head->next) return true;
-    ListNode* slow = head;
-    ListNode* fast = head;
-    while(fast->next && fast->next->next){
-        slow= slow->next;
-        fast=fast->next->next;
+bool isPalindrome(vector<int> &arr, int n){
+    int i = 0;
+    int j = n - 1;
+    while(i < j){
+        if(arr[i] != arr[j]) return false;
+        i++;
+        j--;
     }
+    return true;
 }
 int main(){
-    cout<<"Hello World"<<endl;
+    vector<int> arr{1,2,3,2,1,3};
+    if(isPalindrome(arr, arr.size())){
+        cout<<"True";
+    }
+    else{
+        cout<<"False" << endl;
+    }
+    return 0;
 }
